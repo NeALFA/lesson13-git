b77003ed7d428977a3725db7f1dd2771baeb061f
NeAlfa <sharipovdevelopper@gmail.com>
2023-04-27 03:09:14 -0700

    izoh

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ let modeBtn = document.getElementById("dark-light");
 
 modeBtn.addEventListener("click", function () {
   if (document.body.className != "light") {
-    this.firstElementChild.src = "../assets/images/light.svg";
+    this.firstElementChild.src = "../assets/images/dark.svg";
   } else {
     this.firstElementChild.src = "../assets/images/light.svg";
   }
